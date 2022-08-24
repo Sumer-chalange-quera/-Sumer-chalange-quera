f75ddf03e0cf0e3b9d2ba9857cc3416a0d54ca72
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-08-24 18:47:47 +0430

    Update code.cpp

--- thirdday/prime numbers/code.cpp
+++ thirdday/prime numbers/code.cpp
@@ -10,8 +10,8 @@ bool is_prime(int n)
 # correct code
 bool is_prime(int n)
 {
-    if(n==1 OR n==0):
-        return true;
+    if(n==1 || n==0)
+        return false;
     
     for (int i = 2; i < n; i++)
         if (n % i == 0)
