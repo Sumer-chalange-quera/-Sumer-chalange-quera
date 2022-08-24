e14966871fc6f4d52d15ee9fc0dae0110cb7e230
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2022-08-24 18:49:04 +0430

    Update code.py

--- thirdday/prime numbers/code.py
+++ thirdday/prime numbers/code.py
@@ -1,4 +1,14 @@
+# Wrong code
 def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+# correct code
+def is_prime(n):
+    if(n==0 OR n==1):
+        return False
     for i in range(2, n):
         if n % i == 0:
             return False
